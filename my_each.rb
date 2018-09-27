e1e5b3f4c55f29fc08b86898aeebce13ca91f165
Solee Lee <carolinesoleelee@gmail.com>
2018-09-27 15:00:05 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+
+  while i < array.length
+    i = i + 1
+  end  
+end
+
+
+
+def my_each(array)
+  array.collect do |x|
+    "#{x}"
+end
 end
